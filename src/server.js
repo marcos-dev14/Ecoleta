40f850ec3d52314272ed28d90a57101fae48077a
Marcos Paulo <mprogramador.10@gmail.com>
2020-06-20 12:31:37 -0300

    Finalizado

---
+++ src/server.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const server = express();
+
+// Pegar o banco de dados
+const db = require("./database/db");
+
+// configurar pasta publica
+server.use(express.static("public"));
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }));
+
+
+// utilizando template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
+
+
+// configurar caminhos da minha aplicação
+// pagina inicial
+// req: É um pedido
+// res: É uma resposta
+
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título" });
+});
+
+
+server.get("/create-point", (req, res) => {
+
+    //req.query: Query Strings da nossa url
+    //console.log(req.query)
+
+
+    return res.render("create-point.html")
+});
+
+server.post("/savepoint", (req, res) => {
+    //req.body: o corpo do formulário
+    //console.log(req.body)
+
+    // Inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro de cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso!!")
+        console.log(this)
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+    
+});
+
+
+server.get("/search", (req, res) => {
+
+
+    const search = req.query.search
+
+    if(search == "") {
+        // pesquisa vazia
+        return res.render("search-results.html", { total: 0});
+    }
+
+
+
+    // Pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+    // Mostrar a pagina html com os dados do bando de dados    
+    return res.render("search-results.html", { places: rows, total: total});
+        
+    });
+
+});
+
+
+// Ligar o Servidor
+server.listen(3000);
